7d800e9b7cc394218b32bfa88f3fece6fc326c66
Lucas <sacramento.15@hotmail.com>
2022-08-16 20:56:00 -0300

    Comentando a classe de repositorio de endereco.

--- include/repositorio/RepositorioEndereco.hpp
+++ include/repositorio/RepositorioEndereco.hpp
@@ -5,12 +5,20 @@
 class RepositorioEndereco
 {
   private:
+    // MAP UTILIZADO PARA ARMAZENAR O OBJETO ENDEREÇO UTILIZANDO O CPF COMO CHAVE
     std::map <std::string, Endereco> enderecos;
     std::map <std::string, Endereco>::iterator it;
   public:
-    RepositorioEndereco(/* args */);
+    // CONSTRUTOR
+    RepositorioEndereco();
+
+    // FUNÇÃO RESPONSÁVEL POR CADASTRAR OS ENDEREÇOS
     void cadastrarEndereco(std::string cpf, Endereco& endereco);
+    
+    // FUNÇÃO RESPONSÁVEL POR EXIBIR TODAS AS INFORMAÇÕES DO USUÁRIO
     void exibirEndereco(std::string cpf);
+    
+    // DESTRUTOR
     virtual ~RepositorioEndereco();
 };
 
--- src/repositorio/RepositorioEndereco.cpp
+++ src/repositorio/RepositorioEndereco.cpp
@@ -1,11 +1,14 @@
 #include "RepositorioEndereco.hpp"
 
+// CONSTRUTOR
 RepositorioEndereco::RepositorioEndereco(/* args */){}
 
+// FUNÇÃO RESPONSÁVEL POR CADASTRAR OS ENDEREÇOS
 void RepositorioEndereco::cadastrarEndereco(std::string cpf, Endereco& endereco){
     enderecos.insert(std::pair <std::string, Endereco> (cpf, endereco));
 }
 
+// FUNÇÃO RESPONSÁVEL POR EXIBIR TODAS AS INFORMAÇÕES DO USUÁRIO
 void RepositorioEndereco::exibirEndereco(std::string cpf){
   for(it = enderecos.begin(); it != enderecos.end(); it++){
     if(it -> first == cpf){
@@ -14,4 +17,5 @@ void RepositorioEndereco::exibirEndereco(std::string cpf){
   }
 }
 
+// DESTRUTOR
 RepositorioEndereco::~RepositorioEndereco(){}
